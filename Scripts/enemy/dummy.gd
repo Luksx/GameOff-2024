a47219fc5fa1d622df427fc121e538feca534dcb
Luksx <luxa.jakub@gmail.com>
2024-11-03 20:19:49 +0100

    godot subfolder

--- Scripts/enemy/dummy.gd
+++
@@ -1,9 +0,0 @@
-extends CharacterBody2D
-@onready var hplabel = $hplabel
-
-@export var hp: int = 5
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	hplabel.text = str(hp)
-func get_damage(damage):
-	hp -= damage
