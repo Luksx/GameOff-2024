d079ebaf1959875472d926fb652866948867bf38
zedzee <carsonuderitz@gmail.com>
2024-11-07 09:05:24 -0500

    chain attack updated

--- godot/bosses/chained/attacks/chain_swing/chain_swing.gd
+++ godot/bosses/chained/attacks/chain_swing/chain_swing.gd
@@ -4,7 +4,8 @@ extends Node2D
 const LINK: PackedScene = preload("res://bosses/chained/attacks/chain_swing/chain_link.tscn")
 
 func _ready() -> void:
-	swing(1, 10)
+	swing_alternating(1, 10, 3)
+
 
 func _spawn_links(link_count: int) -> void:
 	for child in get_children():
@@ -22,12 +23,27 @@ func _spawn_links(link_count: int) -> void:
 		add_child(link)
 
 
-func swing(swing_duration: float, link_count: int) -> void:
+func swing(swing_duration: float, link_count: int, spin_count: int) -> void:
 	_spawn_links(link_count)
 
 	var tween := get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
-	tween.tween_property(self, "rotation", 2 * PI, swing_duration)
+	tween.tween_property(self, "rotation", 2 * PI * spin_count, swing_duration * spin_count)
 	await tween.finished
+
+	for child in get_children():
+		child.queue_free()
+
+
+func swing_alternating(swing_duration: float, link_count: int, spin_count: int) -> void:
+	_spawn_links(link_count)
+
+	var dir := 1
+	for i in range(spin_count):
+		print_debug("guh")
+		dir *= -1
+		var tween := get_tree().create_tween().set_ease(Tween.EASE_IN_OUT).set_trans(Tween.TRANS_QUAD)
+		tween.tween_property(self, "rotation", dir * 2 * PI, swing_duration * spin_count)
+		await tween.finished
 
 	for child in get_children():
 		child.queue_free()
